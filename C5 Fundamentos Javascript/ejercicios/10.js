ebca07434519e42b962a7df63f84926f99c47aff
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-02 23:17:41 -0600

    me quede ela 10

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -1,8 +1,21 @@
 function esFechaValida(fecha) {
-  // La función recibe un argumento "fecha".
-  // Comprueba si este corresponde a una fecha válida.
-  // Si es así, retorna true, sino retorna false.
-  // Tu código:
-}
+      // La función recibe un argumento "fecha".
+      // Comprueba si este corresponde a una fecha válida.
+      // Si es así, retorna true, sino retorna false.
+      // Tu código:
+      // let regExp = /^\d{4}-\d{2}-\d{2}$/;
+    
+      let dato = new Date(fecha.getDay());
+      // let somos = fecha;
+      // let obtencion = typeof fecha;
+      // let tipo = typeof fecha;
+      document.write(dato);
+//       if (fecha === dato ) {
+//             return true;
+//       } else {
+//             return false;
+//       }
+// }
+esFechaValida();
 
 module.exports = esFechaValida;
