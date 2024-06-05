7160fe12214a0abad6f61f57e7953644a53719b0
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-05 17:17:01 -0600

    subida hasta el 15

--- C5 Fundamentos Javascript/ejercicios/13.js
+++ C5 Fundamentos Javascript/ejercicios/13.js
@@ -3,11 +3,8 @@ function sonIguales(x, y) {
   // Retorna true si "x" e "y" son iguales.
   // De lo contrario, retorna false.
   // Tu código:
-  if (x === y){
-    return true;
-  }else{
-    return false;
-  }
+  return  x === y;
+
 }
 
 module.exports = sonIguales;
