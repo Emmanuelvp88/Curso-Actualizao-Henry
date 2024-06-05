3e4516bb0e979cc2c9294aa9f3bab9bb006d4b7c
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-05 16:38:54 -0600

    Termine 12 y 13 pendientes el 10  y 11

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -3,19 +3,21 @@ function esFechaValida(fecha) {
       // Comprueba si este corresponde a una fecha válida.
       // Si es así, retorna true, sino retorna false.
       // Tu código:
-      // let regExp = /^\d{4}-\d{2}-\d{2}$/;
-    
-      let dato = new Date(fecha.getDay());
+      let regExp = /^\d{4}-\d{2}-\d{2}$/;
+      var dato = new Date(fecha);
+      // dato.test();
+      
+      // return regExp.test(fecha);
       // let somos = fecha;
       // let obtencion = typeof fecha;
-      // let tipo = typeof fecha;
-      document.write(dato);
-//       if (fecha === dato ) {
-//             return true;
-//       } else {
-//             return false;
-//       }
-// }
+      let tipo = typeof dato;
+      document.write(fecha);
+      // if (regExp.test(fecha)) {
+            //       return true;
+            // } else {
+      //       return false;
+      // }
+}
 esFechaValida();
 
 module.exports = esFechaValida;
