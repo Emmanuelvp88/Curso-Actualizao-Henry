2890bcd8f9564000a58ca3c8f97d71c944cdadde
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-09 20:57:09 -0600

    modificados desde el 16 al 20

--- C5 Fundamentos Javascript/ejercicios/17.js
+++ C5 Fundamentos Javascript/ejercicios/17.js
@@ -2,6 +2,8 @@ function esPar(num) {
   // Retorna true si "num" es par.
   // De lo contrario, retorna false.
   // Tu código:
+  let resultado = num % 2;
+  return resultado == 0;
 }
 
 module.exports = esPar;
--- C5 Fundamentos Javascript/ejercicios/18.js
+++ C5 Fundamentos Javascript/ejercicios/18.js
@@ -2,6 +2,9 @@ function esImpar(num) {
   // Retorna true si "num" es impar.
   // De lo contrario, retorna false.
   // Tu código:
+
+  let resultado = num % 2;
+  return resultado == 1;
 }
 
 module.exports = esImpar;
