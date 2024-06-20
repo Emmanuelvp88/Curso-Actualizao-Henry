285c66e76b7097e6dca064a03f4b4782198cf988
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-20 10:13:57 -0600

    termiinada tarea 35 aprobada con test

--- C5 Fundamentos Javascript/ejercicios/35.js
+++ C5 Fundamentos Javascript/ejercicios/35.js
@@ -3,7 +3,7 @@
 function elevarAlCubo(num) {
   // Retorna el valor de "num" elevado al cubo.
   // Tu código:
-  return Math.pow(num);
+  return Math.pow(num, 3);
 }
 
 module.exports = elevarAlCubo;
