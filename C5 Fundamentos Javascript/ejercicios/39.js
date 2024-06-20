6b7133dabe1b320c9e2465dfbd23b5f82b0ce36f
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-06-20 11:23:54 -0600

    Subidas las ultimas tareas 38, 39 Curso basico terminado

--- C5 Fundamentos Javascript/ejercicios/39.js
+++ C5 Fundamentos Javascript/ejercicios/39.js
@@ -2,7 +2,8 @@
 
 function numeroRandom() {
   // Genera un número al azar entre 0 y 1 y retórnalo.
-  // Tu código:
+  // Tu código:+
+  return Math.random(0, 1);
 }
 
 module.exports = numeroRandom;
